ceb0d8f63f8d900139c5765394693e2978b2a80f
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-28 23:43:05 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/ModalidadeModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/ModalidadeModel.java
@@ -1,5 +1,6 @@
 package br.com.esportes.gestao_competicoes_api.modulo_competicao;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,10 +12,11 @@ import lombok.Setter;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
-@Table(name = "competicao")
+@Table(name = "modalidade")
 public class ModalidadeModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
 
     @Column(name="nome")
@@ -22,6 +24,7 @@ public class ModalidadeModel {
 
     @ManyToOne
     @JoinColumn(name = "competicao_id")
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private CompeticaoModel competicao;
 
 }
