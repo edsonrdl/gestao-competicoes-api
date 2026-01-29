f685336c2b9194417add489647b59d3dfbaeabda
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-28 23:43:02 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/ModalidadeController.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/ModalidadeController.java
@@ -1,4 +1,27 @@
 package br.com.esportes.gestao_competicoes_api.modulo_competicao;
 
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/competicoes")
 public class ModalidadeController {
+
+
+    private final ModalidadeService modalidadeService;
+
+    public ModalidadeController(ModalidadeService modalidadeService) {
+        this.modalidadeService = modalidadeService;
+    }
+
+    @PostMapping("/{idCompeticao}/modalidades")
+    public ResponseEntity<ModalidadeModel> criarModalidade(
+            @PathVariable Long idCompeticao,
+            @RequestBody @Valid ModalidadeModel modalidade) {
+
+        ModalidadeModel novaModalidade = modalidadeService.salvarModalidade(idCompeticao, modalidade);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novaModalidade);
+    }
 }
