08326b25cbafe08e297263f5269752088dab5bbc
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-29 10:12:10 -0300

    Atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoService.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoService.java
@@ -14,4 +14,20 @@ public class CompeticaoService {
     public CompeticaoModel salvarCompeticao(CompeticaoModel competicaoModel) {
         return competicaoRepository.save(competicaoModel);
     }
+    public CompeticaoModel buscarCompeticaoPorId(Long idCompeticao) {
+        return competicaoRepository.findById(idCompeticao)
+                .orElseThrow(() -> new RuntimeException("Competição não encontrada!"));
+    }
+
+    public CompeticaoModel atualizarCompeticao(Long idCompeticao, CompeticaoModel competicaoNovosDados) {
+        CompeticaoModel competicaoExistente = competicaoRepository.findById(idCompeticao)
+                .orElseThrow(() -> new RuntimeException("Competição não encontrada com ID: " + idCompeticao));
+
+        competicaoExistente.setNome(competicaoNovosDados.getNome());
+        competicaoExistente.setRegulamento(competicaoNovosDados.getRegulamento());
+        competicaoExistente.setDataInicio(competicaoNovosDados.getDataInicio());
+        competicaoExistente.setDataFim(competicaoNovosDados.getDataFim());
+
+        return competicaoRepository.save(competicaoExistente);
+    }
 }
