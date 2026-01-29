ab63e8cc80776f8115a2c27586768e882d2f99c4
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-28 23:24:57 -0300

    Atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/ModalidadeModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/ModalidadeModel.java
@@ -1,13 +1,23 @@
 package br.com.esportes.gestao_competicoes_api.modulo_competicao;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "competicao")
 public class ModalidadeModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name="nome")
     private String nome;
 
     @ManyToOne
