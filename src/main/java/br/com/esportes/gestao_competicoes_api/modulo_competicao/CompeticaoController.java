1ecb2906d964594bbcac108ab3a1e79d8469de62
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-28 23:24:30 -0300

    Atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoController.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoController.java
@@ -1,4 +1,28 @@
 package br.com.esportes.gestao_competicoes_api.modulo_competicao;
 
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/competicoes")
 public class CompeticaoController {
+
+    private CompeticaoService competicaoService;
+
+    public CompeticaoController(CompeticaoService competicaoService) {
+        this.competicaoService = competicaoService;
+    }
+
+    @PostMapping("/salvar-competicao")
+    public ResponseEntity<CompeticaoModel> salvarCompeticao(@Valid @RequestBody CompeticaoModel competicaoModel){
+        CompeticaoModel competicaoSalva = competicaoService.salvarCompeticao(competicaoModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(competicaoSalva);
+
+    }
 }
