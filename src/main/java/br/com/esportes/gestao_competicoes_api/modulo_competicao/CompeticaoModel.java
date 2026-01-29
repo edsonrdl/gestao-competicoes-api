91430001a1d9d2e6082a8fa9c6b70d6e84f4dd33
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-28 22:37:07 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoModel.java
@@ -1,24 +1,36 @@
 package br.com.esportes.gestao_competicoes_api.modulo_competicao;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.util.List;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@Table(name = "competicao")
 public class CompeticaoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(name = "regulamento")
     private String regulamento;
 
+    @Column(name = "data_inicio")
     private LocalDate dataInicio;
 
+    @Column(name = "datafim")
     private LocalDate dataFim;
 
     @OneToMany(mappedBy = "competicao")
