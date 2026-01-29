29e02bcb80fb6081d5642bb7a7968a863175f029
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-28 23:24:47 -0300

    Atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoService.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_competicao/CompeticaoService.java
@@ -1,4 +1,17 @@
 package br.com.esportes.gestao_competicoes_api.modulo_competicao;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class CompeticaoService {
+
+    private  final CompeticaoRepository competicaoRepository;
+
+    public CompeticaoService(CompeticaoRepository competicaoRepository) {
+        this.competicaoRepository = competicaoRepository;
+    }
+
+    public CompeticaoModel salvarCompeticao(CompeticaoModel competicaoModel) {
+        return competicaoRepository.save(competicaoModel);
+    }
 }
