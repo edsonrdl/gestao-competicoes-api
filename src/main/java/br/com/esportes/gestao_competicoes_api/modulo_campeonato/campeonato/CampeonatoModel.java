ef354181add1bd09ef618e704d6295e4d743fa44
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 16:04:25 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_campeonato/campeonato/CampeonatoModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_campeonato/campeonato/CampeonatoModel.java
@@ -1,5 +1,6 @@
 package br.com.esportes.gestao_competicoes_api.modulo_campeonato.campeonato;
 
+import br.com.esportes.gestao_competicoes_api.modulo_campeonato.campeonato.regulamento.RegulamentoCampeonatoModel;
 import br.com.esportes.gestao_competicoes_api.modulo_campeonato.modalidade.ModalidadeModel;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -28,10 +30,6 @@ public class CampeonatoModel {
     @Schema(description = "Nome oficial do evento", example = "Campeonato Municipal de Verão 2026")
     private String nome;
 
-    @Column(name = "regulamento")
-    @Schema(description = "Regras básicas ou link para documento", example = "O campeonato seguirá as regras oficiais da FIFA. Fase de grupos seguida de mata-mata.")
-    private String regulamento;
-
     @Column(name = "data_inicio")
     @Schema(description = "Data de início", example = "2026-02-15", type = "string")
     private LocalDate dataInicio;
@@ -39,6 +37,10 @@ public class CampeonatoModel {
     @Column(name = "data_fim")
     @Schema(description = "Data de término", example = "2026-03-30", type = "string")
     private LocalDate dataFim;
+
+    @OneToMany(mappedBy = "campeonato", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Schema(description = "Lista de regras do campeonato",accessMode = Schema.AccessMode.READ_ONLY)
+    private List<RegulamentoCampeonatoModel> regulamentos = new ArrayList<>();
 
     @OneToMany(mappedBy = "campeonato", fetch = FetchType.LAZY)
     @Schema(accessMode = Schema.AccessMode.READ_ONLY)
