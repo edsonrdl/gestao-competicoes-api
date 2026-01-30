320e621512ae70ead225f5dd63ffc4348c318599
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 00:31:59 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/ContatoModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/ContatoModel.java
@@ -1,4 +1,32 @@
 package br.com.esportes.gestao_competicoes_api.modulo_inscricao;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "contato")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ContatoModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "O formato do email é inválido")
+    @Column(nullable = false)
+    private String email;
+
+    @Column(name = "telefone_principal", nullable = false)
+    private String telefonePrincipal;
+
+    @Column(name = "telefone_secundario")
+    private String telefoneSecundario;
+
 }
