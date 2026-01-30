ccaadcdf31c59b7d1487712d1ae061b40edbd5eb
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 00:32:03 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/InscricaoModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/InscricaoModel.java
@@ -1,32 +1,50 @@
 package br.com.esportes.gestao_competicoes_api.modulo_inscricao;
 
 import br.com.esportes.gestao_competicoes_api.modulo_campeonato.modalidade.ModalidadeModel;
+import br.com.esportes.gestao_competicoes_api.modulo_inscricao.equipe.EquipeModel;
 import br.com.esportes.gestao_competicoes_api.modulo_sorteio.GrupoModel;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Entity
+@Table(name = "inscricao")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class InscricaoModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "equipa_id")
-    private EquipeModel equipa;
+    @Column(nullable = false)
+    private LocalDate dataInscricao = LocalDate.now();
 
-    // Alterado de Competicao para Modalidade para maior precisão
+    private String status = "PENDENTE";
+
+    @Column(nullable = false)
+    private boolean cabecaDeChave = false;
+
+
     @ManyToOne
-    @JoinColumn(name = "modalidade_id")
+    @JoinColumn(name = "equipe_id", nullable = false)
+    @JsonIgnoreProperties("historicoParticipacoes")
+    private EquipeModel equipe;
+
+    @ManyToOne
+    @JoinColumn(name = "modalidade_id", nullable = false)
+    @JsonIgnoreProperties("inscricoes")
     private ModalidadeModel modalidade;
 
-    private boolean cabecaDeChave; // Para o algoritmo de sorteio
-
     @ManyToOne
-    @JoinColumn(name = "grupo_id") // Preenchido após o sorteio ser realizado
+    @JoinColumn(name = "grupo_id")
+    @JsonIgnoreProperties("equipasDoGrupo")
     private GrupoModel grupo;
-
-    private LocalDateTime dataInscricao;
 }
-
