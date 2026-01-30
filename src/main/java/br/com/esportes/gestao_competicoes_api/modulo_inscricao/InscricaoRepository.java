f61a95cd22299cd60b1e786ab0c40465b5cc8bb3
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 00:32:08 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/InscricaoRepository.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/InscricaoRepository.java
@@ -1,4 +1,11 @@
 package br.com.esportes.gestao_competicoes_api.modulo_inscricao;
 
-public interface InscricaoRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface InscricaoRepository extends JpaRepository<InscricaoModel,Long> {
+    List<InscricaoModel> findByModalidadeId(Long modalidadeId);
 }
