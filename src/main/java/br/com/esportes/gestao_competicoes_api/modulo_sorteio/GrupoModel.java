197b61cb2a0f3033ee395214743d48dbbb364dfb
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 00:32:31 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_sorteio/GrupoModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_sorteio/GrupoModel.java
@@ -2,25 +2,41 @@ package br.com.esportes.gestao_competicoes_api.modulo_sorteio;
 
 import br.com.esportes.gestao_competicoes_api.modulo_campeonato.modalidade.ModalidadeModel;
 import br.com.esportes.gestao_competicoes_api.modulo_inscricao.InscricaoModel;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Table(name = "grupo") // Define o nome da tabela no banco
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class GrupoModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String nome;
 
     @ManyToOne
-    @JoinColumn(name = "modalidade_id")
+    @JoinColumn(name = "modalidade_id", nullable = false)
+    @JsonIgnoreProperties({"grupos", "inscricoes", "competicao"})
     private ModalidadeModel modalidade;
 
     @Column(columnDefinition = "TEXT")
     private String logAuditoriaSorteio;
 
-    @OneToMany(mappedBy = "grupo")
-    private List<InscricaoModel> equipasDoGrupo;
+
+    @OneToMany(mappedBy = "grupo", fetch = FetchType.EAGER)
+    @JsonIgnoreProperties("grupo")
+    private List<InscricaoModel> equipasDoGrupo = new ArrayList<>();
 }
