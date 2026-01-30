4d03784b65118cb09796b62440cd3bfc596d1a5e
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 02:04:44 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/inscricao/InscricaoController.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/inscricao/InscricaoController.java
@@ -1,4 +1,24 @@
 package br.com.esportes.gestao_competicoes_api.modulo_inscricao.inscricao;
 
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/inscricoes")
 public class InscricaoController {
+
+    private final InscricaoService inscricaoService;
+    public InscricaoController(InscricaoService inscricaoService) {
+        this.inscricaoService = inscricaoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<InscricaoModel> inscreverTime(
+            @RequestParam Long idEquipe,
+            @RequestParam Long idModalidade) {
+
+        InscricaoModel inscricao = inscricaoService.realizarInscricao(idEquipe, idModalidade);
+        return ResponseEntity.ok(inscricao);
+    }
 }
