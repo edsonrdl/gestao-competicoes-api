7c6df27554dab12f134885a99c10168e43100029
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 00:32:36 -0300

    atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_sorteio/GrupoRepository.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_sorteio/GrupoRepository.java
@@ -1,4 +1,9 @@
 package br.com.esportes.gestao_competicoes_api.modulo_sorteio;
 
-public interface GrupoRepository {
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GrupoRepository  extends JpaRepository<GrupoModel, Long> {
 }
