6479f9afbb6e7830fecefd049524c59c5bcff87c
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 01:37:35 -0300

    Atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_recurso/RecursoModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_recurso/RecursoModel.java
@@ -2,35 +2,55 @@ package br.com.esportes.gestao_competicoes_api.modulo_recurso;
 
 import br.com.esportes.gestao_competicoes_api.modulo_campeonato.campeonato.CampeonatoModel;
 import br.com.esportes.gestao_competicoes_api.modulo_inscricao.equipe.EquipeModel;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Table(name = "recurso")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class RecursoModel {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
 
-    @Column(columnDefinition = "TEXT")
+    @Column(nullable = false, columnDefinition = "TEXT")
+    @Schema(description = "Motivo detalhado do recurso", example = "Solicito revisão do resultado do jogo devido à escalação irregular do time adversário.")
     private String descricao;
 
     @Enumerated(EnumType.STRING)
-    private StatusRecursoEnum status;
+    @Column(nullable = false)
+    @Schema(description = "Status atual do processo", example = "PENDENTE")
+    private StatusRecursoEnum status = StatusRecursoEnum.PENDENTE;
 
-    @Column(columnDefinition = "TEXT")
-    private String parecerComissao;
+    @Column(name = "data_solicitacao", nullable = false)
+    private LocalDateTime dataSolicitacao = LocalDateTime.now();
 
-    private LocalDateTime dataSolicitacao;
-
+    @Column(name = "data_analise")
     private LocalDateTime dataAnalise;
 
-    @ManyToOne
-    @JoinColumn(name = "equipa_id")
-    private EquipeModel equipaSolicitante;
+    @Column(name = "parecer_comissao", columnDefinition = "TEXT")
+    @Schema(description = "Resposta oficial da comissão (preenchido na análise)", example = "Recurso aceito. O time adversário perdeu os pontos.")
+    private String parecerComissao;
 
     @ManyToOne
-    @JoinColumn(name = "competicao_id")
-    private CampeonatoModel competicao;
+    @JoinColumn(name = "equipe_id", nullable = false)
+    @JsonIgnoreProperties({"atletas", "historicoParticipacoes", "documentacao", "contato"})
+    private EquipeModel equipeSolicitante;
+
+    @ManyToOne
+    @JoinColumn(name = "campeonato_id", nullable = false)
+    @JsonIgnoreProperties({"modalidades", "regulamento", "tabelaJogos"})
+    private CampeonatoModel campeonato;
 }
-
