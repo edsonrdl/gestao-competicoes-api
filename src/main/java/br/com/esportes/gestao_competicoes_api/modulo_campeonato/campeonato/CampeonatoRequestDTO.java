fcb1b935f967f8ea15c8c04fa9d0dfa6a5e9a5d1
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 22:12:47 -0300

    Atualizado

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_campeonato/campeonato/CampeonatoRequestDTO.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_campeonato/campeonato/CampeonatoRequestDTO.java
@@ -1,4 +1,18 @@
 package br.com.esportes.gestao_competicoes_api.modulo_campeonato.campeonato;
 
-public record CampeonatoRequestDTO() {
-}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+import java.time.LocalDate;
+
+@Schema(description = "Objeto de transferência para criação e atualização de campeonatos")
+public record CampeonatoRequestDTO(
+
+        @Schema(description = "Nome oficial do evento", example = "Campeonato Municipal de Verão 2026")
+        String nome,
+
+        @Schema(description = "Data de início do campeonato", example = "2026-02-15")
+        LocalDate dataInicio,
+
+        @Schema(description = "Data de término do campeonato", example = "2026-03-30")
+        LocalDate dataFim
+) {}
