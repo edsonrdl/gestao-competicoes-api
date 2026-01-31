8e1c3bfd0255f3da9181b039b59aa963668ed023
EDSON-DESKTOP\edson <edsonrodrigo11111@gmail.com>
2026-01-30 23:37:40 -0300

    atualizar

--- src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/equipe/ContatoModel.java
+++ src/main/java/br/com/esportes/gestao_competicoes_api/modulo_inscricao/equipe/ContatoModel.java
@@ -18,17 +18,10 @@ public class ContatoModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Schema(accessMode = Schema.AccessMode.READ_ONLY) // O usuário não envia ID
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY)
     private Long id;
 
-    @Email(message = "O formato do email é inválido")
-    @Column(nullable = false)
-    @Schema(description = "E-mail oficial de contato", example = "diretoria@tabajarafutebol.com.br")
-    private String email;
 
-    @Column(name = "telefone_principal", nullable = false)
-    @Schema(description = "Celular ou WhatsApp principal", example = "(11) 99876-5432")
-    private String telefonePrincipal;
 
     @Column(name = "telefone_secundario")
     @Schema(description = "Telefone para recado ou fixo (Opcional)", example = "(11) 3322-1100")
